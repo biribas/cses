5a2ae3b17dcde7c824a63d79aecd30b4736c7f07
Carlos Adriano <cadrianogamac@gmail.com>
2024-10-25 10:23:11 -0300

    Refactor Nested Ranges Count solution

--- sorting-and-searching/nested-ranges-count.cpp
+++ sorting-and-searching/nested-ranges-count.cpp
@@ -7,8 +7,6 @@ typedef struct {
   pair<int, int> interval;
 } range;
 
-typedef bool(*compare_func)(const range&, const range&);
-
 bool sort_by_first(const range &a, const range &b) {
   if (a.interval.first != b.interval.first) {
     return a.interval.first < b.interval.first;
@@ -23,7 +21,7 @@ bool sort_by_second(const range &a, const range &b) {
   return a.interval.first > b.interval.first;
 }
 
-void merge(vector<range> &ranges, int l, int mid, int r, vector<int> &count, compare_func cmp) {
+void merge(vector<range> &ranges, int l, int mid, int r, vector<int> &contains, vector<int> &contained) {
   int n1 = mid - l + 1;
   int n2 = r - mid;
   
@@ -33,31 +31,39 @@ void merge(vector<range> &ranges, int l, int mid, int r, vector<int> &count, com
   for (int i = 0; i < n2; i++)
     R[i] = ranges[mid + 1 + i];
   
+  int contained_count = 0;
   int i = 0, j = 0, k = l;
   while (i < n1 && j < n2) {
-    if (cmp(L[i], R[j])) {
+    if (sort_by_first(L[i], R[j])) {
+      contained[L[i].index] += contained_count;
       ranges[k++] = L[i++];
     }
     else {
-      count[R[j].index] += n1 - i;
+      contains[R[j].index] += n1 - i;
       ranges[k++] = R[j++];
+      contained_count++;
     }
   }
   
-  while (i < n1) ranges[k++] = L[i++];
-  while (j < n2) ranges[k++] = R[j++];
+  while (i < n1) {
+    contained[L[i].index] += contained_count;
+    ranges[k++] = L[i++];
+  }
+  while (j < n2) {
+    ranges[k++] = R[j++];
+  }
 }
 
-void merge_sort_aux(vector<range> &ranges, int l, int r, vector<int> &count, compare_func cmp) {
+void merge_sort_aux(vector<range> &ranges, int l, int r, vector<int> &contains, vector<int> &contained) {
   if (l >= r) return;
   int mid = l + (r - l) / 2;
-  merge_sort_aux(ranges, l, mid, count, cmp);
-  merge_sort_aux(ranges, mid + 1, r, count, cmp);
-  merge(ranges, l, mid, r, count, cmp);
+  merge_sort_aux(ranges, l, mid, contains, contained);
+  merge_sort_aux(ranges, mid + 1, r, contains, contained);
+  merge(ranges, l, mid, r, contains, contained);
 }
 
-void merge_sort(vector<range> &ranges, vector<int> &count, compare_func cmp) {
-  merge_sort_aux(ranges, 0, ranges.size() - 1, count, cmp);
+void merge_sort(vector<range> &ranges, vector<int> &contains, vector<int> &contained) {
+  merge_sort_aux(ranges, 0, ranges.size() - 1, contains, contained);
 }
 
 int main(void) {
@@ -74,14 +80,11 @@ int main(void) {
   }
   sort(ranges.begin(), ranges.end(), sort_by_second);
 
-  vector<int>contains(n);
-  merge_sort(ranges, contains, sort_by_first);
+  vector<int>contains(n), contained(n);
+  merge_sort(ranges, contains, contained);
   
   for (auto v : contains) cout << v << " ";
   cout << endl;
-
-  vector<int>contained(n);
-  merge_sort(ranges, contained, sort_by_second);
 
   for (auto v : contained) cout << v << " ";
   cout << endl;
