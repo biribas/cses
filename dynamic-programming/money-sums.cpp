9e4e9914c551e3293e8a590636c3689c6ce55e47
Carlos Adriano <cadrianogamac@gmail.com>
2025-06-25 10:48:32 -0300

    Refactor Money Sums solution to use Dynamic Programming

--- dynamic-programming/money-sums.cpp
+++ dynamic-programming/money-sums.cpp
@@ -6,21 +6,30 @@ int main(void) {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n;
+  int n, max_sum = 0;
   cin >> n;
 
-  vector<int> values(n);
-  for (auto &v : values) {
-    cin >> v;
+  vector<int> values(n + 1);
+  for (int i = 1; i <= n; i++) {
+    cin >> values[i];
+    max_sum += values[i];
   }
 
-  set<int> answer;
-  for (auto v : values) {
-    vector<int> original(answer.begin(), answer.end());
-    for (auto x : original) {
-      answer.insert(v + x);
+  vector<vector<bool>> dp(n + 1, vector<bool>(max_sum + 1, false));
+  dp[0][0] = true;
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j <= max_sum; j++) {
+      dp[i][j] = dp[i - 1][j];
+      if (j >= values[i]) {
+        dp[i][j] = dp[i][j] || dp[i - 1][j - values[i]];
+      }
     }
-    answer.insert(v);
+  }
+
+  vector<int> answer;
+  for (int j = 1; j <= max_sum; j++) {
+    if (dp[n][j]) answer.push_back(j);
   }
 
   cout << answer.size() << endl;
